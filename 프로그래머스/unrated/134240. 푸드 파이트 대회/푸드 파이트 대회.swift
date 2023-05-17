18b0d9835309fd6c2df5e017abf47344ddab0779
Jeong-Dong <eejxdong99@gmail.com>
2023-05-18 07:50:21 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.swift
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.swift
@@ -1,10 +1,8 @@
 import Foundation
 func solution(_ food:[Int]) -> String {
-    var arr: [String] = ["0"]
-    for i in (1..<food.count).reversed() {
-        arr.insert((String(repeating: "\(i)", count: food[i]/2)), at: 0)
-        arr.append(String(repeating: "\(i)", count: food[i]/2))
+    var result = ""
+    for i in 1..<food.count {
+        result += String(repeating: "\(i)", count: food[i] / 2)
     }
-    
-    return arr.reduce("", +)
+    return result + "0" + result.reversed()
 }
