c049ef5e1020271005586ab40c50a6c3b8b7564c
Jeong-Dong <eejxdong99@gmail.com>
2023-05-20 10:00:58 +0900

    [unrated] Title: 과일 장수, Time: 0.11 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.swift
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.swift
@@ -1,17 +1,9 @@
 import Foundation
 
 func solution(_ k:Int, _ m:Int, _ score:[Int]) -> Int {
-    var result = 0
-    var reminder = 0
-    var sortedScore = score.sorted(by: {$0 > $1})
+    var sortedScore = score.sorted(by: >)
     
-    for i in (1...k).reversed() {
-        var a = sortedScore.filter({$0 == i})
-        if (a.count + reminder) >= m {
-            result += (i * m * ((a.count + reminder) / m))
-        }
-        reminder = (a.count + reminder) % m
-    }
-    
-    return result
+    return stride(from: m-1, to: score.count, by: m).reduce(0, {
+        $0 + (m * sortedScore[$1])
+    })
 }
