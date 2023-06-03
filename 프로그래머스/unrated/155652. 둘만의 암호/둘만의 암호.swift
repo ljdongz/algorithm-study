8e1aa8683c26f5b120063531630882c613614dd5
Jeong-Dong <eejxdong99@gmail.com>
2023-06-03 16:43:17 +0900

    [unrated] Title: 둘만의 암호, Time: 0.13 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.swift
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.swift
@@ -1,19 +1,12 @@
 import Foundation
 
 func solution(_ s:String, _ skip:String, _ index:Int) -> String {
+    var str = Array("abcdefghijklmnopqrstuvwxyz".filter({ !skip.contains($0) }))
     var result = ""
+    
     for c in s {
-        var asc = UnicodeScalar(String(c))!.value
-        for _ in 1...index {
-            asc += 1
-            if asc == 123 { asc = 97 } 
-            while(skip.contains(String(UnicodeScalar(asc)!))) {
-                asc += 1
-                if asc == 123 { asc = 97 } 
-            }
-            if asc == 123 { asc = 97 } 
-        }
-        result += String(UnicodeScalar(asc)!)
+        var idx = str.firstIndex(of: c)! + index
+        result += (idx >= str.count) ? String(str[idx % str.count]) : String(str[idx])
     }
     
     return result
