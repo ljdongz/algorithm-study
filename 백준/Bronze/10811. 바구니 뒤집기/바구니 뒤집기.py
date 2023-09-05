0e0e743f088a13a795d4eb8358402d9c25afdb44
Jeong-Dong <eejxdong99@gmail.com>
2023-09-05 20:31:13 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -1,15 +1,13 @@
 n, m = map(int, input().split())
 
-lst = []
-for i in range(1, n + 1):
-  lst.append(i)
+lst = [i for i in range(1, n + 1)]
 
 for i in range(0, m):
   a, b = map(int, input().split())
-  tmp = lst.copy()
 
-  for j in range(0, b - a + 1):
-    lst[a - 1 + j] = tmp[b - 1 - j]
-    
+  tmp = lst[a-1:b]
+  tmp.reverse()
+  lst[a-1:b] = tmp
+
 for i in lst:
   print(i, end=' ')
