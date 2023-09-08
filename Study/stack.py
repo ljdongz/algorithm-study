636bcd4e274bdeda61d6ef7f45e69d2d9e6fd328
Jeong Dong <eejxdong99@gmail.com>
2023-09-08 20:58:12 +0900

    stack

---
+++ Study/stack.py
@@ -0,0 +1,25 @@
+string = {"(": ")", "{": "}", "[": "]"}
+
+
+def validParentheses(value) -> bool:
+  inStack = list(value)
+  outStack = []
+  while(len(inStack) > 0):
+    
+    out = inStack.pop()
+
+    if out not in string:
+      outStack.append(out)
+      continue
+    
+    if len(outStack) == 0:
+      return False
+    
+    result = outStack.pop()
+
+    if string[out] != result:
+      return False
+    
+  return True
+
+print(validParentheses("{()[}"))
