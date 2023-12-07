0ac13486c9a7d2cb332baf83b1c14c69d11c8211
Jeong-Dong <eejxdong99@gmail.com>
2023-12-07 11:58:16 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.py
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.py
@@ -1,21 +1,13 @@
-dict = {0: 1, 1: 2, 2: 4}
-
 def solution(n):
     answer = ""
+
+    number = ['1', '2', '4']
     
-    numbers: list = []
-    n -= 1
+    while n > 0:
+        n -= 1
 
-    if n in dict:
-        return str(dict[n])
+        answer = number[n % 3] + answer
 
-    while n >= 0:
-        numbers.insert(0, n % 3)
-        n = n // 3 - 1
-
-    for num in numbers[:-1]:
-        answer += str(dict[(num)])
-
-    answer += str(dict[numbers[-1] % 3])
+        n //= 3
 
     return answer
