b7ea8b3ad2e32e3ffebff45aa8f249f7ef95f138
Jeong-Dong <eejxdong99@gmail.com>
2023-12-11 15:07:38 +0900

    [level 2] Title: 모음 사전, Time: 340.21 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,49 +1,17 @@
 def solution(word):
     answer = 0
-    
-    words = ["A", "E", "I", "O", "U"]
-    wordList = list(word)
 
-    length = len(word)
-    
-    lst = [""]
-    for i in range(5):
-        answer += 1
-        lst[-1] = words[i]
-        if lst == wordList:
-            return answer
-        
-        lst.append("")
-        for j in range(5):
-            answer += 1
-            lst[-1] = words[j]
-            if lst == wordList:
-                return answer
-            
-            lst.append("")
-            for k in range(5):
-                answer += 1
-                lst[-1] = words[k]
-                if lst == wordList:
-                    return answer
-                
-                lst.append("")
-                for l in range(5):
-                    answer += 1
-                    lst[-1] = words[l]
-                    if lst == wordList:
-                        return answer
-                    
-                    lst.append("")
-                    for m in range(5):
-                        answer += 1
-                        lst[-1] = words[m]
-                        if lst == wordList:
-                            return answer
-                        
-                    lst = lst[:-1]
-                lst = lst[:-1]
-            lst = lst[:-1]
-        lst = lst[:-1]
+    words = ["A", "E", "I", "O", "U", ""]
 
+    wordsList = []
 
+    for i in words:
+        for j in words:
+            for k in words:
+                for l in words:
+                    for m in words:
+                        w = i+j+k+l+m
+                        if w not in wordsList: wordsList.append(w)
+
+    wordsList.sort()
+    return wordsList.index(word)
