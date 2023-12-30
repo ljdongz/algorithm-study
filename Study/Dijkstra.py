b5de15fac40209f05111e5ceb75fd9ac46f7a205
Jeong Dong <eejxdong99@gmail.com>
2023-12-31 01:28:56 +0900

    Dijkstra

--- Study/Dijkstra.py
+++ Study/Dijkstra.py
@@ -16,15 +16,16 @@ def dijkstra(graph, start, final):
     costs = {} # 노드에 대한 비용
     pq = [] # priority queue
 
-    heapq.heappush(pq, (0, start)) # 1. 우선순위 큐에 시작 노드 추가
+    heapq.heappush(pq, (0, start, [start])) # 1. 우선순위 큐에 시작 노드 추가
 
     while pq:
-        cur_cost, cur_v = heapq.heappop(pq) # 2. 우선순위가 가장 높은 노드 추출
+        cur_cost, cur_v, cur_route = heapq.heappop(pq) # 2. 우선순위가 가장 높은 노드 추출
         if cur_v not in costs: # 3. 방문 여부 확인
-            costs[cur_v] = cur_cost # 4. 비용 업데이트
+            costs[cur_v] = (cur_cost, cur_route) # 4. 비용 업데이트
             for cost, next_v in graph[cur_v]: # 5. 현재 노드와 연결된 노드들을 우선순위 큐에 추가
                 next_cost = cur_cost + cost 
-                heapq.heappush(pq, (next_cost, next_v))
+                next_route = cur_route + [next_v]
+                heapq.heappush(pq, (next_cost, next_v, next_route))
     
     return costs[final] # 6. 목적지에 기록된 비용 반환
 
