577e1ae38a57db8dfad5ae2d807603e6f899f9b3
Jeong-Dong <eejxdong99@gmail.com>
2024-01-05 14:26:31 +0900

    Time: 62 ms (76.71%), Space: 18.9 MB (6.13%) - LeetHub

---
+++ LeetCode/0001-two-sum/0001-two-sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        start = 0
+        end = len(nums) - 1
+
+        newNums = []
+        for idx, num in enumerate(nums):
+            newNums.append((num, idx))
+
+        newNums.sort()
+
+
+        while start < end:
+            sum = newNums[start][0] + newNums[end][0]
+
+            if sum == target:
+                return [min(newNums[start][1], newNums[end][1]), max(newNums[start][1], newNums[end][1])]
+            elif sum > target:
+                end -= 1
+            elif sum < target:
+                start += 1
