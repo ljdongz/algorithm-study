4100bd12803ed69b18f818f631cc05adab299488
Jeong-Dong <eejxdong99@gmail.com>
2024-01-15 00:17:14 +0900

    [level Lv.2] Title: 방문 길이, Time: 0.39 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/Lv.2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/Lv.2/49994. 방문 길이/방문 길이.py
@@ -1,8 +1,7 @@
 def solution(dirs):
-    answer = 0
 
     cur_coor = (0, 0)
-    visited = {}
+    visited = set()
     command = {"U": (0,1), "L": (-1,0), "R": (1,0), "D": (0,-1)}
 
     for dir in dirs:
@@ -13,9 +12,7 @@ def solution(dirs):
         if next_x >= -5 and next_x <= 5 and next_y >= -5 and next_y <= 5:
             next_coor = (next_x, next_y)
             route = (min(cur_coor, next_coor), max(cur_coor, next_coor))
-            if route not in visited:
-                answer += 1
-                visited[route] = True
+            visited.add(route)
             cur_coor = next_coor
 
-    return answer
+    return len(visited)
