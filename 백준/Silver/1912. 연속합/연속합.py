ba57f1e04b2371a7d1932f8d99b5742c8d581ed6
Jeong-Dong <eejxdong99@gmail.com>
2024-01-27 16:03:56 +0900

    [Silver II] Title: 연속합, Time: 92 ms, Memory: 38828 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -8,10 +8,9 @@ nums = list(map(int, input().split()))
 
 dp = [0] * N
 dp[0] = nums[0]
-maxNum = dp[0]
+
 
 for i in range(1, N):
     dp[i] = max(dp[i-1] + nums[i], nums[i])
-    maxNum = max(maxNum, dp[i])
 
-print(maxNum)
+print(max(dp))
