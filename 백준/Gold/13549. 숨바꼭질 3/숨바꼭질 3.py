26ecd24b1df08dabb7a3f233619d46247bcc82bf
Jeong-Dong <eejxdong99@gmail.com>
2024-02-13 11:19:30 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 152 ms, Memory: 34296 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -19,6 +19,13 @@ while q:
         print(cur_time)
         break
 
+    """
+    1. 순간이동(*2)는 시간이 소요되지 않기 때문에 우선 순위로 둔다.
+    2. 순간이동을 효율적으로 활용하기 위해 -1을 +1보다 우선순위로 둔다.
+        ex) 4 -> 6인 경우.
+        4 -> 5 -> 6 = 2
+        4 -> 3 -> 6 = 1
+    """
     next = [(cur_time, cur_dot * 2), (cur_time + 1, cur_dot - 1), (cur_time + 1, cur_dot + 1)]
     for next_time, next_dot in next:
         if next_dot >= 0 and next_dot <= 100000 and not visited[next_dot]:
