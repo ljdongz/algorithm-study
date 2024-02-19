e9b79f9018e8ef7fa1cb24e046b93794f3e50815
Jeong-Dong <eejxdong99@gmail.com>
2024-02-19 11:10:35 +0900

    [level 2] Title: 2 x n 타일링, Time: 11.76 ms, Memory: 11.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.py
@@ -0,0 +1,14 @@
+def solution(n):
+
+    dp = [0] * n
+
+    if 1 <= n <= 2:
+        return n % 1000000007
+    
+    dp[0] = 1
+    dp[1] = 2
+    
+    for i in range(2, n):
+        dp[i] = (dp[i-2] + dp[i-1]) % 1000000007
+
+    return dp[n-1]
