9af789e48c67051e42f71b4468f31c148bb5b29c
Jeong Dong <eejxdong99@gmail.com>
2024-02-21 18:26:03 +0900

    ExhaustiveSearch_Backtracking

--- Study/ExhaustiveSearch_Backtracking.py
+++ Study/ExhaustiveSearch_Backtracking.py
@@ -1,86 +1,89 @@
 
-"""
-백트래킹(backtraking)
-- 좁은 의미
-    solution이 될 가능성이 없는 후보는 더 이상 탐색하지 않고 포기(backtrack)하면서 탐색
-- 넓은 의미
-    재귀를 이용하여 모든 경우의 수를 방문하는 것
-"""
+# """
+# 백트래킹(backtraking)
+# - 좁은 의미
+#     solution이 될 가능성이 없는 후보는 더 이상 탐색하지 않고 포기(backtrack)하면서 탐색
+# - 넓은 의미
+#     재귀를 이용하여 모든 경우의 수를 방문하는 것
+# """
 
-# 조합 (nPr)
-def permute(nums, k):
-    def backtrack(curr):
-        if len(curr) == k:
-            ans.append(curr[:]) # 복사
-            return
+# # 조합 (nPr)
+# def permute(nums, k):
+#     def backtrack(curr):
+#         if len(curr) == k:
+#             ans.append(curr[:]) # 복사
+#             return
         
-        for i in range(len(nums)):
-            if nums[i] not in curr:
+#         for i in range(len(nums)):
+#             if nums[i] not in curr:
 
-                # 중요 포인트
-                curr.append(nums[i])
-                backtrack(curr)
-                curr.pop()
+#                 # 중요 포인트
+#                 curr.append(nums[i])
+#                 backtrack(curr)
+#                 curr.pop()
 
-    ans = []
-    backtrack([])
-    return ans
+#     ans = []
+#     backtrack([])
+#     return ans
 
-print(permute([1,2,3], 3)) # [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+# print(permute([1,2,3], 3)) # [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
 
 
 
-# 순열 (nCr)
-def combination(nums, k):
-    result = []
+# # 순열 (nCr)
+# def combination(nums, k):
+#     result = []
 
-    def backtraking(start, curr):
-        if len(curr) == k:
-            result.append(curr[:]) # 복사
-            return
+#     def backtraking(start, curr):
+#         if len(curr) == k:
+#             result.append(curr[:]) # 복사
+#             return
         
-        for i in range(start, len(nums)):
-            curr.append(nums[i])
-            backtraking(i+1, curr)
-            curr.pop()
+#         for i in range(start, len(nums)):
+#             curr.append(nums[i])
+#             backtraking(i+1, curr)
+#             curr.pop()
 
-    backtraking(0, [])
-    return result
-
-
-print(combination([1,2,3,4], 2)) # [[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]]
+#     backtraking(0, [])
+#     return result
 
 
+# print(combination([1,2,3,4], 2)) # [[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]]
 
-# 부분집합
-def subset(nums):
 
-    result = []
 
-    def backtraking(curr, start):
-        if len(curr) == k:
-            result.append(curr[:])
-            return
+# # 부분집합
+# def subset(nums):
+
+#     result = []
+
+#     def backtraking(curr, start):
+#         if len(curr) == k:
+#             result.append(curr[:])
+#             return
         
-        for i in range(start, len(nums)):
-            curr.append(nums[i])
-            backtraking(curr, i+1)
-            curr.pop()
+#         for i in range(start, len(nums)):
+#             curr.append(nums[i])
+#             backtraking(curr, i+1)
+#             curr.pop()
 
-    for k in range(len(nums)+1):
-        backtraking([], 0)
+#     for k in range(len(nums)+1):
+#         backtraking([], 0)
 
-    return result
+#     return result
 
-print(subset([1,2,3,4])) # [[], [1], [2], [3], [4], [1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4], [1, 2, 3], [1, 2, 4], [1, 3, 4], [2, 3, 4], [1, 2, 3, 4]]
+# print(subset([1,2,3,4])) # [[], [1], [2], [3], [4], [1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4], [1, 2, 3], [1, 2, 4], [1, 3, 4], [2, 3, 4], [1, 2, 3, 4]]
 
 
 
 # two sum
 def twoSum(nums, k, target):
     def backtrack(start, curr):
-        if len(curr) == k and sum(curr) == target:
-            return curr
+        if len(curr) == k:
+            if sum(curr) == target:
+                return curr
+            else:
+                return None
 
         for i in range(start, len(nums)):
             curr.append(nums[i])
