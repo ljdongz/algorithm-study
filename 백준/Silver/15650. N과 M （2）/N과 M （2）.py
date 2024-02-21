8a51973d5571d857dcd0984538de3c55ae7c8850
Jeong-Dong <eejxdong99@gmail.com>
2024-02-21 11:26:37 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,25 @@
+
+import sys
+
+input = sys.stdin.readline
+
+N, M = list(map(int, input().split()))
+
+result = []
+
+def backtrack(start, curr):
+    if len(curr) == M:
+        result.append(curr[:])
+        return
+    
+    for i in range(start, N+1):
+        curr.append(i)
+        backtrack(i+1, curr)
+        curr.pop()
+
+backtrack(1, [])
+
+for r in result:
+    for c in r:
+        print(c, end=" ")
+    print()
