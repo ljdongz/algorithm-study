2a4dad38d25b5745fe81ea7123f834396964a961
Lee Jeong-Dong <eejxdong99@gmail.com>
2024-03-25 00:43:30 +0900

    [Silver III] Title: 블로그2, Time: 84 ms, Memory: 44588 KB -BaekjoonHub

--- 백준/Silver/20365. 블로그2/블로그2.py
+++ 백준/Silver/20365. 블로그2/블로그2.py
@@ -6,26 +6,8 @@ N = int(input())
 
 colors = input().rstrip()
 
-answer = N
+reds = [c for c in colors.split("R") if c]
+blues = [c for c in colors.split("B") if c]
 
-for c in ["B", "R"]:
-    totalCount = 1
-    diff = False
-
-    for color in colors:
-        # 비교하는 색상이랑 다를 경우
-        if c != color:
-            diff = True
-        # 비교하는 색상이랑 같을 경우
-        else:
-            # 이전에 다른 색상이 나온 경우
-            if diff:
-                totalCount += 1
-                diff = False
-    
-    if diff:
-        totalCount += 1
-    answer = min(answer, totalCount)
-
-print(answer)
-    
+result = min(len(reds), len(blues)) + 1
+print(result)
