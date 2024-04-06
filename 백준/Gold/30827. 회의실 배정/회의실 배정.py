d049187a76edbeeb7158d1690877f9b35ad87a41
Lee Jeong-Dong <eejxdong99@gmail.com>
2024-04-07 00:47:41 +0900

    [Gold IV] Title: 회의실 배정, Time: 688 ms, Memory: 86788 KB -BaekjoonHub

--- 백준/Gold/30827. 회의실 배정/회의실 배정.py
+++ 백준/Gold/30827. 회의실 배정/회의실 배정.py
@@ -9,17 +9,16 @@ meetings = [list(map(int, input().split())) for _ in range(N)]
 meetings.sort(key=lambda x: (x[1], x[0]))
 
 # 회의실 (회의 종료 시간을 담음)
-rooms = [[0] for _ in range(K)]
+rooms = [0] * K
 
 result = 0
 
 for meeting in meetings:
-    rooms.sort(key=lambda x: x[-1], reverse=True)
-    for room in rooms:
-        if room[-1] < meeting[0]:
-            room.append(meeting[1])
+    rooms.sort(reverse=True)
+    for idx in range(len(rooms)):
+        if rooms[idx] < meeting[0]:
+            rooms[idx] = meeting[1]
             result += 1
             break
 
 print(result)
-
