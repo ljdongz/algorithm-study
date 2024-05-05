38d050bd858f3578d396cc289b1e7058407ec819
Lee Jeong-Dong <eejxdong99@gmail.com>
2024-05-05 12:57:25 +0900

    [Gold IV] Title: 부분합, Time: 132 ms, Memory: 42204 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -0,0 +1,31 @@
+import sys
+
+input = sys.stdin.readline
+
+N, S = list(map(int, input().split()))
+
+nums = list(map(int, input().split()))
+
+start = end = 0
+cur_sum = 0
+result = 100000
+
+while end < N:
+    if cur_sum < S:
+        cur_sum += nums[end]
+        end += 1
+    else:
+        result = min(result, end - start)
+        cur_sum -= nums[start]
+        start += 1
+
+if cur_sum >= S:
+    while cur_sum >= S:
+        result = min(result, end - start)
+        cur_sum -= nums[start]
+        start += 1
+    print(result)
+elif start != 0:
+    print(result)
+else:
+    print(0)
