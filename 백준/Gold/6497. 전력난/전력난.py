9129809a21fa561d5da3254ce884522af85dbdce
Lee Jeong-Dong <eejxdong99@gmail.com>
2024-05-12 00:47:21 +0900

    [Gold IV] Title: 전력난, Time: 2296 ms, Memory: 62432 KB -BaekjoonHub

--- 백준/Gold/6497. 전력난/전력난.py
+++ 백준/Gold/6497. 전력난/전력난.py
@@ -2,40 +2,43 @@ import sys
 import heapq
 from collections import defaultdict
 
+sys.setrecursionlimit(100000)
+
 input = sys.stdin.readline
 
-while True:
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
 
+def union_parent(parent, x, y):
+    rootX = find_parent(parent, x)
+    rootY = find_parent(parent, y)
+    parent[rootX] = rootY
+
+while True:
     m, n = list(map(int, input().split()))
 
     if m == n == 0:
         break
 
-    houses = defaultdict(list)
-    visited = [False] * m
-
     result = 0
 
+    parent = [i for i in range(m)]
+
+    cities = []
     for _ in range(n):
-        x, y, z = list(map(int, input().split()))
-        houses[x].append((y, z))
-        houses[y].append((x, z))
-        result += z
+        x, y, cost = list(map(int, input().split()))
+        cities.append((cost, x, y))
+        result += cost
+    cities.sort()
 
-    hq = []
-    heapq.heappush(hq, (0, 0)) # 비용, 도착지
+    for cost, x, y in cities:
+        rootX = find_parent(parent, x)
+        rootY = find_parent(parent, y)
 
-    while hq:
-        cur_cost, cur_house = heapq.heappop(hq)
-
-        if visited[cur_house]:
-            continue
-
-        visited[cur_house] = True
-        result -= cur_cost
-
-        for next_house, next_cost in houses[cur_house]:
-            if next_cost != -1 and not visited[next_house]:
-                heapq.heappush(hq, (next_cost, next_house))
+        if rootX != rootY:
+            result -= cost
+            union_parent(parent, rootX, rootY)
 
     print(result)
