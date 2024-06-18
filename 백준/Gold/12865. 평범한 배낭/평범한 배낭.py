e0b9b44f458f42d4742a6432cc0bf62af9822826
Lee Jeong-Dong <eejxdong99@gmail.com>
2024-06-18 10:50:57 +0900

    [Gold V] Title: 평범한 배낭, Time: 4512 ms, Memory: 229084 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -4,32 +4,18 @@ input = sys.stdin.readline
 
 N, K = list(map(int, input().split()))
 
-products = []
-
-for _ in range(N):
-    products.append(list(map(int, input().split()))) 
-    
-products.sort()
+products = [list(map(int, input().split())) for _ in range(N)]
 
 memo = [[0] * (K+1) for _ in range(N+1)]
 
-maxValue = 0
-
-for row, product in enumerate(products):
+for i, product in enumerate(products):
     weight = product[0]
     value = product[1]
 
-    for col in range(1, K+1):
-        if col < weight:
-            memo[row+1][col] = memo[row][col]
-        elif col == weight:
-            memo[row+1][col] = max(memo[row][col], value)
+    for j in range(1, K+1):
+        if j < weight:
+            memo[i+1][j] = memo[i][j]
         else:
-            if memo[row][col - weight] != 0:
-                memo[row+1][col] = max(memo[row][col - weight] + value, memo[row][col])
-            else:
-                memo[row+1][col] = memo[row][col]
-                
-        maxValue = max(maxValue, memo[row+1][col])
+            memo[i+1][j] = max(memo[i][j], memo[i][j-weight] + value)
 
-print(maxValue)
+print(memo[-1][-1])
