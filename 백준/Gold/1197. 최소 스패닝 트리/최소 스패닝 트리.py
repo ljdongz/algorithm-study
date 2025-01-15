61c90f23237d243e81e4da970f023b66a8bdc215
Lee Jeong-Dong <eejxdong99@gmail.com>
2025-01-15 23:35:24 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 380 ms, Memory: 56808 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,33 +1,33 @@
 import sys
-import heapq
-from collections import defaultdict
-
+sys.setrecursionlimit(10000000)
 input = sys.stdin.readline
 
 V, E = list(map(int, input().split()))
 
-visited = [False] * (V + 1)
-graph = defaultdict(list)
+parents = [i for i in range(V+1)]
+graph = []
+result = 0
 
 for _ in range(E):
   v1, v2, w = list(map(int, input().split()))
-  graph[v1].append((v2, w))
-  graph[v2].append((v1, w))
+  graph.append((w, (v1, v2)))
+graph.sort()
 
-hq = []
-heapq.heappush(hq, (0, 1))
+def find_parent(x):
+  if parents[x] != x:
+    parents[x] = find_parent(parents[x])
+  return parents[x]
 
-result = 0
+def union(x, y):
+  xNode = find_parent(x)
+  yNode = find_parent(y)
+  parents[xNode] = yNode
 
-while hq:
-  cur_weight, cur_node = heapq.heappop(hq)
-
-  if not visited[cur_node]:
-    result += cur_weight
-    visited[cur_node] = True
-
-    for (next_node, next_weight) in graph[cur_node]:
-      if not visited[next_node]:
-        heapq.heappush(hq, (next_weight, next_node))
+for weight, (v1, v2) in graph:
+  rootX = find_parent(v1)
+  rootY = find_parent(v2)
+  if rootX != rootY:
+    result += weight
+    union(v1, v2)
 
 print(result)
