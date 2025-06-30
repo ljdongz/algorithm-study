9ff04b66bf6bbcf4b3688aa8e3e331abd9ab374e
Lee Jeong-Dong <eejxdong99@gmail.com>
2025-06-30 16:19:58 +0900

    [level 0] Title: 옹알이 (1), Time: 1.72 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.swift
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.swift
@@ -1,20 +1,45 @@
 import Foundation
 
+/*
+
+
+
+*/
+
 func solution(_ babbling:[String]) -> Int {
     let words = ["aya", "ye", "woo", "ma"]
     var count = 0
     
+    var combines: [String] = []
+    
+    for i in 1...4 {
+        
+        for x in 0..<words.count {
+            
+            for y in 0..<words.count {
+                if i == 1 || x == y { continue }
+                
+                for z in 0..<words.count {
+                    if i == 2 || x == z || y == z { continue }
+                    
+                    for w in 0..<words.count {
+                        if i == 3 || x == w || y == w || z == w { continue }
+                        
+                        combines.append(words[x] + words[y] + words[z] + words[w])
+                    }
+                    
+                    combines.append(words[x] + words[y] + words[z])
+                }
+                
+                combines.append(words[x] + words[y])
+            }
+            
+            combines.append(words[x])
+        }
+    }
+    
     for bab in babbling {
-        var bab = bab
-        
-        for word in words {
-            let split = bab.components(separatedBy: word).joined(separator: " ")
-            bab = split
-        }
-        
-        if bab.trimmingCharacters(in: .whitespaces).isEmpty { 
-            count += 1
-        }
+        if combines.contains(bab) { count += 1 }
     }
     
     return count
