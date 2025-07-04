c3dbd43ad44c0711fe16c4bd5ced0b6051cb112b
Lee Jeong-Dong <eejxdong99@gmail.com>
2025-07-04 22:59:40 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.11 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.swift
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.swift
@@ -4,12 +4,10 @@ func solution(_ dots:[[Int]]) -> Int {
     
     let sortedDots = dots.sorted { ($0[0], $0[1]) < ($1[0], $1[1]) }
     
-    let a = sortedDots[0]
-    let b = sortedDots[1]
-    let c = sortedDots[2]
+    let x = sortedDots[0]
+    let y = sortedDots[3]
     
-    let x = sqrt(pow(Double(a[0] - b[0]), 2) + pow(Double(a[1] - b[1]), 2))
-    let y = sqrt(pow(Double(a[0] - c[0]), 2) + pow(Double(a[1] - c[1]), 2))
     
-    return Int(x * y)
+    
+    return (y[0] - x[0]) * (y[1] - x[1])
 }
