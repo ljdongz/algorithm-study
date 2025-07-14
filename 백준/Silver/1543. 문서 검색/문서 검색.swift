efff378b70bff17a9e20516f2a4f23f6b4a1cd5d
Lee Jeong-Dong <eejxdong99@gmail.com>
2025-07-14 12:33:47 +0900

    [Silver V] Title: 문서 검색, Time: 8 ms, Memory: 79504 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.swift
+++ 백준/Silver/1543. 문서 검색/문서 검색.swift
@@ -3,5 +3,5 @@ import Foundation
 let document = readLine()!
 let word = readLine()!
 
-let components = document.components(separatedBy: word).filter { $0 != "" }.joined(separator: "")
+let components = document.components(separatedBy: word).joined(separator: "")
 print((document.count - components.count) / word.count)
