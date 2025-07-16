8544063b49834a72e774489c6d085618ae2f66c6
Lee Jeong-Dong <eejxdong99@gmail.com>
2025-07-16 14:34:32 +0900

    [Silver III] Title: N과 M (6), Time: 16 ms, Memory: 79516 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+let n = input[0]
+let m = input[1]
+
+let numbers = readLine()!.split(separator: " ").map { Int(String($0))! }.sorted()
+
+var visited: [Bool] = .init(repeating: false, count: n + 1)
+var current: [Int] = []
+
+func recursive(k: Int, index: Int) {
+  if k == m {
+    // 출력
+    for i in 0..<m {
+      print("\(current[i])", terminator: " ")
+    }
+    print()
+    return
+  }
+
+  for i in index..<n {
+    if !visited[i] {
+      visited[i] = true
+      current.append(numbers[i])
+      recursive(k: k + 1, index: i + 1)
+      current.removeLast()
+      visited[i] = false
+    }
+  }
+}
+
+recursive(k: 0, index: 0)
