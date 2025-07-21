49380e6f9fcb384d4858d15c8d8fa71d5ec000e8
Lee Jeong-Dong <eejxdong99@gmail.com>
2025-07-21 11:01:31 +0900

    [Silver II] Title: 부분수열의 합, Time: 76 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.swift
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.swift
@@ -11,23 +11,23 @@ let target = input[1]
 let numbers = readLine()!.split(separator: " ").map { Int(String($0))! }
 var result = 0
 
-// k: 현재까지 뽑은 숫자 개수
-// s: 시작점
-// m: 뽑아야 하는 숫자 개수
+// total: 뽑아야 하는 숫자 개수
+// cur: 현재까지 뽑은 숫자 개수
+// start: 시작점
 // sum: 현재까지 합
-func recursive(k: Int, s: Int, m: Int, sum: Int) {
-  if k == m {
+func recursive(total: Int, cur: Int, start: Int, sum: Int) {
+  if cur == total {
     if sum == target { result += 1 }
     return
   }
 
-  for i in s..<n {
-    recursive(k: k + 1, s: i + 1, m: m, sum: sum + numbers[i])
+  for i in start..<n {
+    recursive(total: total, cur: cur + 1, start: i + 1, sum: sum + numbers[i])
   }
 }
 
 for i in 1...n {
-  recursive(k: 0, s: 0, m: i, sum: 0)
+  recursive(total: i, cur: 0, start: 0, sum: 0)
 }
 
 print(result)
