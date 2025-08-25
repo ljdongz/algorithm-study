7f635ff27fddc86094d9b0831993f4428bc6fa68
Lee Jeong-Dong <eejxdong99@gmail.com>
2025-08-25 10:25:25 +0900

    [Silver I] Title: 미로 탐색, Time: 12 ms, Memory: 80056 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.swift
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.swift
@@ -4,8 +4,8 @@ let input = readLine()!.split(separator: " ").map { Int(String($0))! }
 let n = input[0]
 let m = input[1]
 
-var visited: [[Bool]] = .init(
-  repeating: .init(repeating: false, count: m), 
+var visited: [[Int]] = .init(
+  repeating: .init(repeating: 0, count: m), 
   count: n
 )
 var drdc = [(0, 1), (0, -1), (1, 0), (-1, 0)]
@@ -16,28 +16,26 @@ for _ in 0..<n {
   board.append(input)
 }
 
-var queue: [(Int, Int, Int)] = []
-queue.append((0, 0, 1))
+var queue: [(Int, Int)] = []
+queue.append((0, 0))
 var front = 0
-visited[0][0] = true
+visited[0][0] = 1
 
 while queue.count >= front + 1 {
-  let (curR, curC, count) = queue[front]
+  let (curR, curC) = queue[front]
   front += 1
-
-  if curR == n - 1 && curC == m - 1 {
-    print(count)
-  }
 
   for (dr, dc) in drdc {
     let nextR = curR + dr
     let nextC = curC + dc
 
     if nextR >= 0 && nextR < n && nextC >= 0 && nextC < m {
-      if !visited[nextR][nextC] && board[nextR][nextC] == 1 {
-        queue.append((nextR, nextC, count + 1))
-        visited[nextR][nextC] = true
+      if visited[nextR][nextC] == 0 && board[nextR][nextC] == 1 {
+        queue.append((nextR, nextC))
+        visited[nextR][nextC] = visited[curR][curC] + 1
       }
     }
   }
 }
+
+print(visited[n - 1][m - 1])
