3f4a0dbf97cc392ea86cc4e74a9413c826d0e0df
Lee Jeong-Dong <eejxdong99@gmail.com>
2025-09-08 11:37:42 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.04 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.swift
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.swift
@@ -2,9 +2,6 @@ import Foundation
 
 func solution(_ n:Int, _ w:Int, _ num:Int) -> Int {
     
-    let dir = [(0, 1), (1, 0), (0, -1), (1, 0)]
-    var dirIndex = 0
-    
     let rowCount = (n / w) + (n % w == 0 ? 0 : 1)
     var board: [[Int]] = .init(
         repeating: .init(
@@ -16,36 +13,31 @@ func solution(_ n:Int, _ w:Int, _ num:Int) -> Int {
     var targetIndex = (0, 0)
     var result = 0
     
-    var row = 0
-    var col = 0
-    for number in 1...n {
-        board[row][col] = number
+    var number = 1
+    out: for row in 0..<board.count {
         
-        if number == num { targetIndex = (row, col) }
+        let isForward = row % 2 == 0
         
-        var (r, c) = dir[dirIndex]
-        var nextR = row + r
-        var nextC = col + c
-        
-        if nextC < 0 || nextC >= w {
-            dirIndex = (dirIndex + 1) % 4
-            (r, c) = dir[dirIndex]
-            nextR = row + r
-            nextC = col + c
-            dirIndex = (dirIndex + 1) % 4
+        for col in 0..<w {
+            if number > n { break out }
+            
+            let r = row
+            let c = isForward ? col : (w - col - 1)
+            
+            board[r][c] = number
+            
+            if number == num { targetIndex = (r, c) }
+            
+            number += 1
         }
-        
-        row = nextR
-        col = nextC
     }
-    
+ 
     while targetIndex.0 < board.count {
         let row = targetIndex.0
         let col = targetIndex.1
-        targetIndex = (row + 1, col)
         if board[row][col] != 0 { result += 1 }
+        targetIndex = (row + 1, col)
     }
     
     return result
-    
 }
