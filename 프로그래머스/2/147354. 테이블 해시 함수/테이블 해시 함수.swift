99ae1752438556b5d2408a4f6ddf4be551d4f969
Lee Jeong-Dong <eejxdong99@gmail.com>
2025-09-16 12:05:22 +0900

    [level 2] Title: 테이블 해시 함수, Time: 90.20 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.swift
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.swift
@@ -8,39 +8,21 @@ func solution(_ data:[[Int]], _ col:Int, _ row_begin:Int, _ row_end:Int) -> Int 
         return first < second
     }
     
-    var bits: [[Int]] = []
+    var bits: [Int] = []
     
     for row in row_begin...row_end {
-        var mod = 0
+        var bit = 0
         for col in sortedData[row - 1] {
-            mod += col % row
+            bit += col % row
         }
         
-        var bit: [Int] = []
-        for i in 0..<20 {
-            bit.append(mod % 2)
-            mod /= 2
-        }
-        bits.append(bit.reversed())
-    }
-    
-    if bits.isEmpty { return 0 }
-    
-    var resultBit: [Int] = .init(repeating: 0, count: 20)
-    for bit in bits {
-        var temp: [Int] = []
-        
-        for i in 0..<20 {
-            temp.append(resultBit[i] == bit[i] ? 0 : 1)
-        }
-        
-        resultBit = temp
+        bits.append(bit)
     }
     
     var result = 0
-
-    for i in 0..<20 {
-        result += Int(pow(Double(2), Double(i))) * resultBit[20 - i - 1]
+    
+    for bit in bits {
+        result ^= bit
     }
     
     return result
